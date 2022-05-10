c55efbd5df1df063c13668355b3e3a4375799211
cham <nchaminn@mail.ru>
2022-05-10 19:32:51 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите числов а: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите числов b: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+  {
+     int max = a;
+     int min = b;
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+  }
+
+else 
+  {
+    int max = b;
+    int min = a;
+   Console.WriteLine(max); Console.WriteLine(min);
+  }
+
+
+
