09f2b2663789ea002398d69a2efab13b2cfe5640
cham <nchaminn@mail.ru>
2022-05-12 06:56:45 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+
+for (i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+ {
+  Console.Write(i + " ");
+ }
+}
